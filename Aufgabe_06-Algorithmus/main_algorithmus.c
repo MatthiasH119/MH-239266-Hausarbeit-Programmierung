9322c35a8817741c407b8d8930760495734b6f9f
MatthiasH119 <matthiasheiming@aol.de>
2022-08-11 20:09:03 +0200

    A06_V00.2_2022-08-11_adding_code

--- Aufgabe_06-Algorithmus/main_algorithmus.c
+++ Aufgabe_06-Algorithmus/main_algorithmus.c
@@ -8,16 +8,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char version[]="V0.1";
+char version[]="V0.2";
 int primzahlsieb[]={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199};
 
 int primzahl_pruefung(int zahl)
 {
-    for (int i=0; i<46; i++)
+    #ifdef DEBUG
+    printf("\x1b[34m//DEBUG-PRIMZAHL-PRÜFUNG: zahl: %i\x1b[0m", zahl);
+    #endif
+    for (int i=0; i<45; i++)
     {
 	if (primzahlsieb[i] == zahl)
+	{
+		#ifdef DEBUG
+		printf("\t\x1b[34m//DEBUG-PRIMZAHL-PRÜFUNG: \x1b[32mreturn 1 \x1b[0m\n");
+		#endif
 		return 1;
+	}
     }
+    #ifdef DEBUG
+    printf("\t\x1b[34m//DEBUG-PRIMZAHL-PRÜFUNG: \x1b[31mreturn 0 \x1b[0m\n");
+    #endif
     return 0;
 }
 
@@ -36,21 +47,31 @@ int main()
     int stelle_max = 0;
     int max = 0;
 
-    for(int i=0; i<200; i++)
+    for(int i=0; i<199; i++)
     {
+	#ifdef DEBUG
+	printf("\x1b[35m i = %i \x1b[30m\n", i);
+	#endif
 	if (primzahl_pruefung(zahlenreihe[i]) == 1)
 	{
-	    for(int j=1; j=1000; j++)
+	    printf("IF-SCHLEIFE \n");
+	    int x = 0;
+	    for(int j=1; x<1; j++)
 	    {
-	    printf("Ihr seid dooooooooooooof \n");
+    	        printf("FOR-SCHLEIFE\n");
+	        #ifdef DEBUG
+	        printf("\x1b[35m j = %i \x1b[30m\n", j);
+	        #endif
 	        if (primzahl_pruefung(zahlenreihe[i+j]) == 0)
 		{
-		    j=1000;
+		    printf("j+i = %i \n", j+i);
 		    if(j>max)
 		    {
+			printf("j > max \n");
 			max = j;
 			stelle_max = i;
 		    }
+		    x=1;
 		}
 	    }
 	}
