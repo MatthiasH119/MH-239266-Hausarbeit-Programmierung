ed1bb32397835c7671cc1ea23805c5c01a7f8225
MatthiasH119 <103685216+MatthiasH119@users.noreply.github.com>
2022-08-24 08:04:52 +0200

    Update zahlengenerator.c

--- Aufgabe_06-Algorithmus/zahlengenerator.c
+++ Aufgabe_06-Algorithmus/zahlengenerator.c
@@ -1,3 +1,12 @@
+//      Autor:          	Cedric-Julian Nettler                   |       Matthias Heiming
+//      Mail:           	cedric-julian.nettler@stud.thga.de      |       matthias.heiming@stud.thga.de
+//      Datum:          	28.06.2022
+//
+//	Angepasst/Überarbeitet:	Matthias Heiming, 09.08.2022
+//
+//      Hausarbeit Matthias Heiming, Aufgabe 06 - Zahlengenerator
+//	Der Zahlengenerator wurde aus dem Praktikum nr. 9 übernommen und für die Aufgabe 06 der Hausarbeit angepasst.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
