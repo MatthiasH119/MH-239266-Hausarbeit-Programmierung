30ecec5c7b0780276fe0283ec708923fcbce15d2
MatthiasH119 <matthiasheiming@aol.de>
2022-09-15 19:01:47 +0200

    A06_V00.2_2022-09-15_astyle

--- Aufgabe_06-Algorithmus/main_algorithmus.c
+++ Aufgabe_06-Algorithmus/main_algorithmus.c
@@ -8,23 +8,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char version[]="V0.5";
-int primzahlsieb[]={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199};
+#define MAX 200      //Hoechste Zahl im Instream
+#define ANZAHL  200  //Anzahl Zahlen im Instream
 
+char version[]="V0.7";
+
+int primzahlliste[MAX];
+
+//Funktion zum erstllen der Primzahlliste
+void primzahlliste_erstellung()
+{
+    int primzahlliste_zaehler = 0;
+    for (int i=2; i<MAX; i++)
+    {
+        char pruefstempel = 't';
+        for (int j=2; j<i; j++)
+        {
+            if (i % j == 0)
+            {
+                pruefstempel = 'f';
+                break;
+            }
+        }
+        if (pruefstempel == 't')
+        {
+            primzahlliste[primzahlliste_zaehler] = i;
+            primzahlliste_zaehler++;
+        }
+    }
+    for (int i=0; i<MAX; i++)
+    {
+        if (primzahlliste[i] == 0)
+            primzahlliste[i] = -1;
+    }
+    #ifdef DEBUG
+    printf("Primzahlliste: {");
+    for (int i; i<MAX; i++)
+    {
+        if (primzahlliste[i] != 0)
+            printf("%i, ", primzahlliste[i]);
+        else
+            break;
+    }
+    printf("} \n");
+    #endif
+}
+
+//Funktion zum pruefen, ob eine Zahl eine Primzahl ist
 int primzahl_pruefung(int zahl)
 {
     #ifdef DEBUG
     printf("\x1b[34m//DEBUG-PRIMZAHL-PRÜFUNG: zahl: %i\x1b[0m", zahl);
     #endif
-    for (int i=0; i<46; i++)
+    for (int i=0; i<MAX; i++)
     {
-	if (primzahlsieb[i] == zahl)
-	{
-		#ifdef DEBUG
-		printf("\t\x1b[34m//DEBUG-PRIMZAHL-PRÜFUNG: \x1b[32mreturn 1 \x1b[0m\n");
-		#endif
-		return 1;
-	}
+        if (primzahlliste[i] == zahl)
+        {
+	    #ifdef DEBUG
+            printf("\t\x1b[34m//DEBUG-PRIMZAHL-PRÜFUNG: \x1b[32mreturn 1 \x1b[0m\n");
+	    #endif
+            return 1;
+        }
+        else if (primzahlliste[i] == -1)
+            break;
     }
     #ifdef DEBUG
     printf("\t\x1b[34m//DEBUG-PRIMZAHL-PRÜFUNG: \x1b[31mreturn 0 \x1b[0m\n");
@@ -34,56 +80,57 @@ int primzahl_pruefung(int zahl)
 
 int main()
 {
+    primzahlliste_erstellung();
     FILE * in_zahlenreihe;
     in_zahlenreihe = fopen("zahlenreihe.txt", "r");
 
-    int zahlenreihe[200];
+    int zahlenreihe[ANZAHL];
 
-    for (int i=0; i<200; i++)
+    for (int i=0; i<ANZAHL; i++)
     {
-	fscanf(in_zahlenreihe, " %i,", &zahlenreihe[i]);
+        fscanf(in_zahlenreihe, " %i,", &zahlenreihe[i]);
     }
 
     int stelle_max = 0;
     int max = 0;
 
-    for(int i=0; i<200; i++)
+    for(int i=0; i<ANZAHL; i++)
     {
 	#ifdef DEBUG
-	printf("\x1b[35m i = %i \x1b[30m\n", i);
+        printf("\x1b[35m i = %i \x1b[30m\n", i);
 	#endif
-	if (primzahl_pruefung(zahlenreihe[i]) == 1)
-	{
-	    int x = 0;
-	    for(int j=1; x<1; j++)
-	    {
-	        #ifdef DEBUG
-	        printf("\x1b[35m j = %i \x1b[30m\n", j);
-	        #endif
-	        if (primzahl_pruefung(zahlenreihe[i+j]) == 0)
-		{
-		    if(j>max)
-		    {
-			max = j;
-			stelle_max = i;
-		    }
-		    i = i + j;
-		    x=1;
-		}
-	    }
-	}
+        if (primzahl_pruefung(zahlenreihe[i]) == 1)
+        {
+            int x = 0;
+            for(int j=1; x<1; j++)
+            {
+		#ifdef DEBUG
+                printf("\x1b[35m j = %i \x1b[30m\n", j);
+		#endif
+                if (primzahl_pruefung(zahlenreihe[i+j]) == 0)
+                {
+                    if(j>max)
+                    {
+                        max = j;
+                        stelle_max = i;
+                    }
+                    i = i + j;
+                    x=1;
+                }
+            }
+        }
     }
 
     if (max < 2)
-	    printf("In der Zahlenreihe gibt es keine Primzahlreihe ! \n");
+        printf("In der Zahlenreihe gibt es keine Primzahlreihe ! \n");
     else
     {
-    	printf("Die längste Reihe von Primzahlen in der Zahlenreihe ist %i Primzahlen lang.\n", max);
-    	printf("Die Zahlen lauten: ");
+        printf("Die längste Reihe von Primzahlen in der Zahlenreihe ist %i Primzahlen lang.\n", max);
+        printf("Die Zahlen lauten: ");
 
-    	for(int i=stelle_max; i < stelle_max+max; i++)
-    	{
-	    printf("%i, ", zahlenreihe[i]);
+        for(int i=stelle_max; i < stelle_max+max; i++)
+        {
+            printf("%i, ", zahlenreihe[i]);
         }
         printf("\n");
     }
--- Aufgabe_06-Algorithmus/zahlengenerator.c
+++ Aufgabe_06-Algorithmus/zahlengenerator.c
@@ -22,22 +22,22 @@
 
 int main() {
 
-        int zufallszahlen[NUMS_TO_GENERATE];
+    int zufallszahlen[NUMS_TO_GENERATE];
 
-        FILE * outstream;
-        outstream = fopen("zahlenreihe.txt", "w");
+    FILE * outstream;
+    outstream = fopen("zahlenreihe.txt", "w");
 
-	
-	srand(time(NULL));
-        for (int i = 0; i < NUMS_TO_GENERATE; i++){
-                zufallszahlen[i] = rand() % MAX;
-        }
 
-        for (int i = 0; i < NUMS_TO_GENERATE; i++){
-                fprintf(outstream, "%d, ", zufallszahlen[i]);
-        }
+    srand(time(NULL));
+    for (int i = 0; i < NUMS_TO_GENERATE; i++) {
+        zufallszahlen[i] = rand() % MAX;
+    }
 
-        fclose(outstream);
+    for (int i = 0; i < NUMS_TO_GENERATE; i++) {
+        fprintf(outstream, "%d, ", zufallszahlen[i]);
+    }
 
-        return 0;
+    fclose(outstream);
+
+    return 0;
 }
